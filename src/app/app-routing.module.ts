878391a152100f21987d9adb35b851dbb3bde7b5
Mohamed Emir Jammeli <mohamedemir.jammeli@advyteam.com>
2020-01-17 12:07:20 +0100

    fixed-version

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {MatiereComponent} from './matiere/matiere.component';
+import {AjoutMatiereComponent} from './ajout-matiere/ajout-matiere.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/matiere', pathMatch: 'full' },
+  { path: 'matiere', component: MatiereComponent },
+  { path: 'ajout-matiere', component: AjoutMatiereComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,69 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MatiereComponent } from './matiere/matiere.component';
+import {StoreModule} from '@ngrx/store';
+import {appEffects, getReducers, REDUCER_TOKEN} from './store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import { AjoutMatiereComponent } from './ajout-matiere/ajout-matiere.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {MatiereService} from './matiere/matiere.service';
+import {HttpClientModule} from '@angular/common/http';
+import {
+  MatButtonModule,
+  MatCardModule,
+  MatGridListModule, MatIconModule, MatListModule,
+  MatMenuModule,
+  MatSidenavModule,
+  MatTableModule,
+  MatToolbarModule
+} from '@angular/material';
+import {ConfirmationPopoverModule} from 'angular-confirmation-popover';
+import {ToastrModule} from 'ngx-toastr';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {LayoutModule} from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MatiereComponent,
+    AjoutMatiereComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    StoreModule.forRoot(REDUCER_TOKEN),
+    EffectsModule.forRoot(appEffects),
+    StoreDevtoolsModule.instrument({
+      name: '[DEMOANGULAR]',
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production // Restrict extension to log-only mode
+    }),
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+    MatTableModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule,
+    ConfirmationPopoverModule,
+    ConfirmationPopoverModule.forRoot({
+      confirmButtonType: 'danger' // set defaults here
+    }), ToastrModule.forRoot(),
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
-  providers: [],
+  providers: [{
+    provide: REDUCER_TOKEN,
+    useFactory: getReducers
+  }, MatiereService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
